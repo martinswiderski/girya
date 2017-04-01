6bb9740a373781353baa9671e7507841cc373c6c
Martin Swiderski <codebloke@gmail.com>
2017-04-01 21:16:38 +0100

    colouring added

--- tests/specs/README.md
+++ tests/specs/README.md
@@ -1,5 +1,5 @@
 MD5 Signatures
 ---
 
- * **[girya-spec.js](girya-spec.js)** (e946aed7e15ea57362fceb791f791050, Sat Apr 1 20:08:46 2017 +0100 ...94cd42ca)
+ * **[girya-spec.js](girya-spec.js)** (11aa9d16ad92abc3a0455a5d3fca32d5, Sat Apr 1 20:08:46 2017 +0100 ...94cd42ca)
 
--- tests/specs/girya-spec.js
+++ tests/specs/girya-spec.js
@@ -1,14 +1,28 @@
 'use strict';
 
-var girya = require('./../../index'),
-    _test = require('./_md5/girya-details.json');
+var girya = require('./../../index');
 
-console.log('MD5: ' + _test.md5 + ' File: ' + _test.file);
+var _test = require('./_md5/girya-details.json'),
+    color = require('bash-color');
+
+console.log('MD5: ' + color.cyan(_test.md5) + ' File: ' + color.yellow(_test.file));
 
 describe('Implements multiple interfaces', function () {
     it('version', function () {
        expect(typeof girya.version).toBe('function');
        expect(girya.version()).toBe('0.0.1');
+    });
+    it('_config', function () {
+       expect(typeof girya._config).toBe('function');
+    });
+    it('package', function () {
+       expect(typeof girya.package).toBe('function');
+       expect(girya.package().version).toBe('0.0.1');
+       expect(girya.package().name).toBe('girya');
+    });
+    it('version', function () {
+       expect(typeof girya.configuration).toBe('object');
+       expect(girya.configuration.id()).toBe(1);
     });
 });
 
